87db5a6a789accc177e0c89c592a4cbea8f5d81a
Arpit Tyagi <arpittyagi102@gmail.com>
2023-08-07 23:23:09 +0530

    adding icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,13 +14,12 @@ export default function Home(){
           objectFit:"cover"
         }}
       />
-      <div className="w-full bg-gray-800 p-4 sticky">
-          <img width="48" height="48" src="https://img.icons8.com/color/48/windows-11.png" alt="windows-11"/>
-      </div>
-      <div>
-          <img width="48" height="48" src="https://img.icons8.com/color/48/windows-explorer.png" alt="windows-explorer"/>
-          <img width="48" height="48" src="https://img.icons8.com/color/48/chrome--v3.png" alt="chrome--v3"/>
-          <img width="48" height="48" src="https://img.icons8.com/fluency/48/spotify.png" alt="spotify"/>
+      <div className="w-full bg-gray-800 p-2 absolute  flex align-center justify-center bottom-0 gap-3">
+          <img width="32" height="32" src="https://img.icons8.com/color/48/windows-11.png" alt="windows-11"/>
+          <img width="32" height="32" src="https://img.icons8.com/color/48/windows-explorer.png" alt="windows-explorer"/>
+          <img width="32" height="32" src="https://img.icons8.com/color/48/chrome--v3.png" alt="chrome--v3"/>
+          <img width="32" height="32" src="https://img.icons8.com/fluency/48/spotify.png" alt="spotify"/>
+          <img width="32" height="32" src="https://img.icons8.com/glyph-neue/64/notion.png" alt="notion"/>
       </div>
     </>
   )
